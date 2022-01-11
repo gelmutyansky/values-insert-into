7b2fe32ea970f52fc82fbf1df7bca9b00a14aa95
gelmutyansky <rgelmut@yandex.ru>
2022-01-11 14:49:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,35 @@ function valuesInsertInto(numOfValues, divider, constant = 0) {
 
     return res.join();
 }
+
+/**
+ * Функция, отдающая VALUES строку для INSERT INTO. Версия 2
+ * @param {Number} paramsLength - общее количество параметров в массиве
+ * @param {Number} allColumns - общее количество колонок, в которые вставляем значения
+ * @param {Number} constantParamsLength - количество констант в начале
+ */
+function valuesInsertIntoV2(paramsLength, allColumns, constantParamsLength = 0){
+    const divider = allColumns - constantParamsLength
+
+    let num = 1 + constantParamsLength;
+    const cycles = (paramsLength - constantParamsLength) / divider;
+    let res = [];
+    for (let i = 1; i <= cycles; i++) {
+        let str = `(`;
+        let dollars = [];
+
+        for (let con = 1; con <= constantParamsLength; con++) {
+            dollars.push(`$${ con }`);
+        }
+
+        for (; num <= (divider * i) + constantParamsLength; num++) {
+            dollars.push(`$${ num }`);
+        }
+
+        str += dollars.join() + `)`;
+
+        res.push(str);
+    }
+
+    return res.join();
+}
